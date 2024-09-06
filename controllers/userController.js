86fcd003b27475c7ec9512278a4f4830555ea68b
Bibhu <sahoo.bibhuprasad740@gmail.com>
2024-09-06 12:53:53 +0530

    fix: fixed daily study study count bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,13 +14,23 @@ exports.ping = async (req, res, next) => {
         if (!user) {
             return APIErrorHandler(res, Errors.USER_NOT_FOUND_ERROR);
         }
-        
+
         const dateDiff = dateUtils.dateDifference(user.lastLogin);
 
-        if(dateDiff !== 0) {
-            user.lastLogin = new Date();
-            user.studyPoints += 1;
-            await user.save();
+        if (dateDiff !== 0) {
+            await User.findOneAndUpdate(
+                { email },
+                {
+                    $set: {
+                        lastLogin: new Date(),
+                        dailyStudyCount: 0,
+                        dailyReviseCount: 0
+                    },
+                    $inc: { studyPoints: 1 }
+                }
+            );
+        } else {
+            console.log('User logged in again!');
         }
         APISuccessHandler(res, Successes.GENERAL_SUCCESS, {
             message: "Ping successful"
